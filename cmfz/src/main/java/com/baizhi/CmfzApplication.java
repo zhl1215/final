f610d147a55e7c16f8ef95fbd042613f4f1ce655
sher <769321919@qq.com>
2018-12-19 19:51:00 +0800

    add jsp

--- cmfz/src/main/java/com/baizhi/CmfzApplication.java
+++ cmfz/src/main/java/com/baizhi/CmfzApplication.java
@@ -2,7 +2,9 @@ package com.baizhi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.baizhi.mapper")
 @SpringBootApplication
 public class CmfzApplication {
 
