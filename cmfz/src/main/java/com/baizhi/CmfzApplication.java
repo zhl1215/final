25b2543818146ca0e435b5419d20a944da356cf5
sher <769321919@qq.com>
2018-12-19 19:42:25 +0800

    init project

---
+++ cmfz/src/main/java/com/baizhi/CmfzApplication.java
@@ -0,0 +1,14 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmfzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmfzApplication.class, args);
+    }
+
+}
+
